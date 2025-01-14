db540079c02a39f2f583adc6ee7bc4b86a4cceab
TapiwaTendayi <tapiwatatenda123@gmail.com>
2025-01-14 08:27:52 +0200

    Inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -48,8 +48,32 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15C </strong>
+              </div>
+              <div class="weather-forecast-temperature">9C</div>
+            </div>
+          </div>
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
+displayForecast();
